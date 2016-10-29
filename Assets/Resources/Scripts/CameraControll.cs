fab7c46a7ff50c862c9d0d2f8874d60069509bee
worminer <worminer@gmail.com>
2016-10-29 20:12:49 +0300

    /Commiting Files that i missed last commit.

---
+++ Assets/Resources/Scripts/CameraControll.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControll : MonoBehaviour {
+
+    public Transform player; //Player Transform
+    public float cameraYOffset;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // make camera folow the player
+        transform.position = new Vector3(player.position.x, player.position.y + cameraYOffset, transform.position.z); // or just use Vector2
+	}
+}
