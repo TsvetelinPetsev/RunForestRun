cc3e1a79bb7a443ee9175e0ab125df27afbbf739
TsvetelinPetsev <tsvetelinpetsev@gmail.com>
2016-11-10 00:51:18 +0200

    added high score

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -5,6 +5,13 @@ using UnityEngine.UI;
 public class CoinManager : MonoBehaviour {
        
     private int money;
+    private int highScore;
+
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("highscore");
+        GameObject.Find("BestTimeScore").GetComponent<Text>().text = highScore.ToString();
+    }
 
     void FixedUpdate()
     {
@@ -12,6 +19,12 @@ public class CoinManager : MonoBehaviour {
         {
             money++;
             GameObject.Find("MoneyTxt").GetComponent<Text>().text = money.ToString();
+        }
+
+        if (money > highScore)
+        {
+            PlayerPrefs.SetInt("highscore", money);
+            PlayerPrefs.Save();
         }
     }
 
@@ -22,7 +35,19 @@ public class CoinManager : MonoBehaviour {
             Debug.Log("Triggered");
             money++;
             GameObject.Find("MoneyTxt").GetComponent<Text>().text = money.ToString();
+
+            GameObject.Find("YourTimeScore").GetComponent<Text>().text = money.ToString();
+            GameObject.Find("BestTimeScore").GetComponent<Text>().text = highScore.ToString();
             Destroy(colision.gameObject);
         }
+    }
+
+    public void ResetScore()
+    {
+        //money = 0;
+        highScore = 0;
+        GameObject.Find("BestTimeScore").GetComponent<Text>().text = highScore.ToString();
+        PlayerPrefs.SetInt("highscore", 0);
+        PlayerPrefs.Save();
     }
 }
